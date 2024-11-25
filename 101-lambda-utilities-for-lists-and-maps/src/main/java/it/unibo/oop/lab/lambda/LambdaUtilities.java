5ee6d10cff1cd2f35297bec68aad8c406cc73fc5
Riccardo Carta <riccardo.carta2@studio.unibo.it>
2024-11-25 11:20:03 +0100

    101: Implemented fill()

--- 101-lambda-utilities-for-lists-and-maps/src/main/java/it/unibo/oop/lab/lambda/LambdaUtilities.java
+++ 101-lambda-utilities-for-lists-and-maps/src/main/java/it/unibo/oop/lab/lambda/LambdaUtilities.java
@@ -116,7 +116,9 @@ public final class LambdaUtilities {
          *
          * Keep in mind that a map can be iterated through its forEach method
          */
-        return null;
+        final Map<K, V> filledMap = new HashMap<>();
+        map.forEach((k,v) -> filledMap.put(k, v.orElse(def.get())));
+        return filledMap;
     }
 
     /**
